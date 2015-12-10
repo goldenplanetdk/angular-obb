f4df54c1f5f0ca6e846b97c5ea7491624cb66bed
Steven Pribilinskiy <steven.pribilinskiy@gmail.com>
2015-12-10 11:30:07 +0200

    feat(forms): new directive: `blurOnEnterKey`

---
+++ forms/directives/blurOnEnterKey.js
@@ -0,0 +1,18 @@
+export default (ngModule) => {
+
+	ngModule.directive('blurOnEnterKey', function(cssUtil) {
+		return {
+			restrict: 'A',
+			link: function(scope, element) {
+
+				element.on('keydown keypress', (event) => {
+
+					if (event.which === 13) {
+
+						element.blur();
+					}
+				});
+			}
+		};
+	});
+}
